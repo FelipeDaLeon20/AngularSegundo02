0600beee08c873b5fdaf8418a8d716b1bcddbbda
IEVN1002-22001429 <79970@alumnos.utleon.edu.mx>
2025-10-10 23:39:12 -0600

    Zodiaco chino sin imagenes

--- src/app/formulario/zodiaco/zodiaco.component.ts
+++ src/app/formulario/zodiaco/zodiaco.component.ts
@@ -9,6 +9,43 @@ import { zodiaco } from './zodiaco';
   styleUrl: './zodiaco.component.css'
 })
 export class ZodiacoComponent {
+
 formularioZodiaco!:FormGroup;
 objZodiaco=new zodiaco();
+edadM!:number;
+signoM!:string;
+nombreM!:string;
+sexoM!:string;
+
+ngOnInit():void
+  {
+    this.formularioZodiaco=new FormGroup
+    (
+      {
+        nombre:new FormControl(''),
+        aPaterno:new FormControl(''),
+        aMaterno:new FormControl(''),
+        dia:new FormControl(''),
+        mes:new FormControl(''),
+        anio:new FormControl(''),
+        sexo:new FormControl(''),
+      }
+    )
+  }
+  imprimirZodiaco()
+  {
+    this.objZodiaco.diaN=this.formularioZodiaco.value.dia;
+    this.objZodiaco.mesN=this.formularioZodiaco.value.mes;
+    this.objZodiaco.anioN=this.formularioZodiaco.value.anio;
+    this.objZodiaco.sexoC=this.formularioZodiaco.value.sexo;
+    this.objZodiaco.calcularEdad();
+    this.edadM=this.objZodiaco.edadV;
+    this.objZodiaco.conocerSignoChino();
+    this.signoM=this.objZodiaco.signo;
+    this.objZodiaco.conocerSexo();
+    this.sexoM=this.objZodiaco.sexoV;
+    this.nombreM=this.formularioZodiaco.value.nombre;
+  }
+
+
 }
--- src/app/formulario/zodiaco/zodiaco.ts
+++ src/app/formulario/zodiaco/zodiaco.ts
@@ -1,4 +1,74 @@
 export class zodiaco
 {
     
+    diaN!:number;
+    mesN!:number;
+    anioN!:number;
+    edadV!:number;
+    numClave!:number;
+    signo!:string;
+    sexoC!:number;
+    sexoV!:string;
+
+    calcularEdad()
+    {
+     const fechaActual = new Date();
+     const FechaNacimiento= new Date(this.anioN,this.mesN,this.diaN);
+     let edad = fechaActual.getFullYear() - FechaNacimiento.getUTCFullYear();
+     const mesActual=fechaActual.getMonth();
+     const mesNacimiento=FechaNacimiento.getMonth();
+     const diaActual=fechaActual.getDay();
+     const diaNacimiento=FechaNacimiento.getDay();
+
+     if(mesNacimiento>mesActual || (mesNacimiento==mesActual && diaNacimiento>diaActual))
+     {
+        edad--;
+     }
+     return this.edadV=edad;
+    }
+    conocerSignoChino()
+    {
+     this.numClave=this.anioN%12;
+     switch(this.numClave)
+     {
+        case 0: this.signo="Mono";
+        break;
+        case 1: this.signo="Gallo";
+        break;
+        case 2: this.signo="Perro";
+        break;
+        case 3: this.signo="Cerdo";
+        break;
+        case 4: this.signo="Rata";
+        break;
+        case 5: this.signo="Buey";
+        break;
+        case 6: this.signo="Tigre";
+        break;
+        case 7: this.signo="Conejo";
+        break;
+        case 8: this.signo="Dragón";
+        break;
+        case 9: this.signo="Serpiente";
+        break;
+        case 10: this.signo="Caballo";
+        break;
+        case 11: this.signo="Cabra";
+        break;
+     }
+     return this.signo;
+
+    }
+    conocerSexo()
+    {
+      if(this.sexoC==0)
+      {
+         this.sexoV="masculino";
+      }
+      else
+      {
+         this.sexoV="femenino";
+      }
+    }
+
 }
